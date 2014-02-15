3794d22f70a6d19f028165511ffbb7c5a5fd1f33
Paul Yeoh <paulyeoh.lz@gmail.com>
2014-02-15 11:13:41 +0700

    add user_pages tests, improve flash message markup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -11,20 +11,49 @@ describe "UserPages" do
 		# Ex 5.6.1 Using Capybara's have_selector
 		# replace:
 		# it { should have_content('Sign up') }
-		# with:
+		# with more precise:
+		# it { should have_selector('h1', text: 'Sign up') }
+
 		it { should have_selector('h1', text: 'Sign up') }
 		it { should have_title(full_title('Sign up')) }
 
-		it "should not sign up a user with incomplete information" do 
-			expect { click_button submit }.not_to change(User, :count)
-		end
+		describe "with invalid information" do 
 
-		it "should sign up a new user with valid information" do 
-			fill_in "Name", with: "Foo Bar"
-			fill_in "Email", with: "foo@bar.com"
-			fill_in "Password", with: "password"
-			fill_in "Confirmation", with: "password"
-			expect { click_button submit }.to change(User, :count).by(1)
+			it "should not sign up a user" do 
+				expect { click_button submit }.not_to change(User, :count)
+			end
+
+			# Ex 7.6.2 Test error messages for user sign-up.
+			describe "displays error messages" do 
+				before { click_button submit }
+
+				it { should have_title('Sign up') }
+				it { should have_selector('div#error_explanation', text: 'error') }
+			end
+		end
+		
+		describe "with valid information" do 
+
+			let(:fill_user_details) do 
+				fill_in "Name", with: "Foo Bar"
+				fill_in "Email", with: "foo@bar.com"
+				fill_in "Password", with: "password"
+				fill_in "Confirmation", with: "password"
+				click_button submit
+			end
+
+			it "should sign up a new user" do 				
+				expect { fill_user_details }.to change(User, :count).by(1)
+			end
+
+			# Ex 7.6.3 Test post user creation.
+			describe "after saving the user" do 
+				before { fill_user_details }
+				let(:user) { User.find_by(email: 'foo@bar.com') }
+
+				it { should have_title(user.name) }
+				it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+			end
 		end
 	end
 
